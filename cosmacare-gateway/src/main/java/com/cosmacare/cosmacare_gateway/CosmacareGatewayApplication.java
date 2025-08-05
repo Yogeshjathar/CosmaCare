f02ccb4732ff0deb7ba8730569eab0edd9313dfa
Yogesh Jathar <109969909+Yogeshjathar@users.noreply.github.com>
2025-08-05 14:52:27 +0530

    Initiating Project CosmaCare

---
+++ cosmacare-gateway/src/main/java/com/cosmacare/cosmacare_gateway/CosmacareGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cosmacare.cosmacare_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmacareGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmacareGatewayApplication.class, args);
+	}
+
+}
