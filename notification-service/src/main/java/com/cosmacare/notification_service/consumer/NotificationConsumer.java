8704b03e835d4583c3671ca834ddd2e627aa8412
Yogesh Jathar <109969909+Yogeshjathar@users.noreply.github.com>
2025-10-10 11:12:18 +0530

    updated parent pom.xml and module pom.xml to handle multi module project effectively

--- notification-service/src/main/java/com/cosmacare/notification_service/consumer/NotificationConsumer.java
+++ notification-service/src/main/java/com/cosmacare/notification_service/consumer/NotificationConsumer.java
@@ -5,6 +5,7 @@ import com.cosmacare.notification_service.entity.Notification;
 import com.cosmacare.notification_service.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -15,23 +16,38 @@ public class NotificationConsumer {
 
     private final NotificationRepository repository;
 
-    @KafkaListener(topics = "repair-created", groupId = "notification-group",  containerFactory = "repairKafkaListenerContainerFactory")
-    public void consumeMessage(RepairCreatedEvent event) {
-        log.info("📩 Notification Service received event for repair: " + event.getId());
+    @KafkaListener(
+            topics = "repair-created",
+            groupId = "notification-group",
+            containerFactory = "repairKafkaListenerContainerFactory"
+    )
+    public void consumeMessage(ConsumerRecord<String, RepairCreatedEvent> record) {
 
-        // Create a simple notification message
+        // Extract data from ConsumerRecord
+        String topic = record.topic();
+        int partition = record.partition();
+        long offset = record.offset();
+        String key = record.key();
+        RepairCreatedEvent event = record.value();
+
+        log.info("📩 Received Kafka message -> topic={}, partition={}, offset={}, key={}, event={}",
+                topic, partition, offset, key, event);
+
+        // Build notification message
         String notificationMsg = String.format(
                 "New repair request created by %s for issue: %s",
                 event.getStoreWorkerUserName(),
                 event.getIssueType()
         );
 
-        // Save Notification
+        // Save Notification to DB
         Notification notification = new Notification();
         notification.setRepairId(event.getId());
         notification.setMessage(notificationMsg);
         notification.setCreatedAt(event.getCreatedAt());
 
         repository.save(notification);
+
+        log.info("✅ Notification saved successfully for repair ID {}", event.getId());
     }
 }
--- security-service/src/main/java/com/cosmacare/security_service/config/SecurityConfig.java
+++ security-service/src/main/java/com/cosmacare/security_service/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/auth/validate", "/swagger-ui/index.html", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/auth/login", "/auth/validate","/api/user/register", "/swagger-ui/index.html", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
